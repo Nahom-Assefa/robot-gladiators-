a0b5c37893a316da4dfda838c117e4938d4dd64b
Nahom Assefa <nzenebe@gmail.com>
2021-10-18 18:58:58 -0500

    first iteration of robot battle

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,25 @@
-window.alert("This is an alert! JavaScript is running!");
+window.alert("The fight has begun!");
+const playerName = "James";
+let playerHealth = 100;
+let playerAttack = 10;
+
+console.log(playerName, playerHealth, playerAttack);
+
+const enemyName = "Roborto";
+let enemyHealth = 50;
+let enemyAttack = 12;
+
+const fight = function() {
+    window.alert("Welcome to Robot Gladiators");
+    playerHealth = playerHealth - enemyAttack;
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+}
+
+fight();
+
+if (playerHealth <= 0) {
+window.alert(playerName + " has died!");
+} 
+else {
+window.alert(playerName + " still has " + playerHealth + " health left.");
+}
